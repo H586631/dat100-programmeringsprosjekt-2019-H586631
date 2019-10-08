23a4510a278c095fb6c323aa20e71c4c1a0d3543
hakonoygard <hakonoygard@158-37-196-17.hvl.no>
2019-10-08 13:55:50 +0200

    formatert riktig

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -1,17 +1,15 @@
 package no.hvl.dat100ptc.oppgave1;
 
-import no.hvl.dat100ptc.TODO;
-
 public class GPSPoint {
 
 	private int time;
 	private double latitude;
 	private double longitude;
-	private double elevation; 
-		
+	private double elevation;
+
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
-		//konstruktur
+		// konstruktur
 
 		this.time = time;
 		this.latitude = latitude;
@@ -19,64 +17,63 @@ public class GPSPoint {
 		this.elevation = elevation;
 
 	}
-	
-	
-	// TODO - get/set metoder
+
+	// get/set metoder
 	public int getTime() {
-		
+
 		return time;
-		
+
 	}
 
 	public void setTime(int time) {
-				
+
 		this.time = time;
 
-	} 
+	}
 
 	public double getLatitude() {
-		
+
 		return latitude;
-		
+
 	}
 
 	public void setLatitude(double latitude) {
-		
+
 		this.latitude = latitude;
-		
+
 	}
 
 	public double getLongitude() {
-		
+
 		return longitude;
-		
+
 	}
 
 	public void setLongitude(double longitude) {
-		
+
 		this.longitude = longitude;
-		
+
 	}
 
 	public double getElevation() {
-		
+
 		return elevation;
-		
+
 	}
 
 	public void setElevation(double elevation) {
-		
+
 		this.elevation = elevation;
-		
+
 	}
-	
+
 	public String toString() {
-		
+
 		String str;
-		
-		str =  time + " (" + latitude + "," + longitude + ") " + elevation+ "\n";
+
+		str = time + " (" + latitude + "," + longitude + ") " + elevation + "\n";
 
 		return str;
-		
+
 	}
 }
