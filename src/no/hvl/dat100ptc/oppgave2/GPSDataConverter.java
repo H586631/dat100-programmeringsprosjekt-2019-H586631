f3dfafe1eddbe87c56bc6a0b51d0322d31f43453
hakonoygard <hakonoygard@158-37-196-68.hvl.no>
2019-10-09 11:12:46 +0200

    Oppgave 2 lagt inn toSeconds og convert

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -1,6 +1,5 @@
 package no.hvl.dat100ptc.oppgave2;
 
-import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSDataConverter {
@@ -14,27 +13,30 @@ public class GPSDataConverter {
 
 	public static int toSeconds(String timestr) {
 		
-		int secs;
-		int hr, min, sec;
+		int hr = Integer.parseInt(timestr.substring(TIME_STARTINDEX,13));
+		int min = Integer.parseInt(timestr.substring(14,16));
+		int sec = Integer.parseInt(timestr.substring(17,19));
 		
-		// TODO
-		// OPPGAVE - START
+		int secs =  (hr * 60 * 60) + (min * 60) + sec;
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return secs;
 
-		// OPPGAVE - SLUTT
 		
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
 
 		GPSPoint gpspoint;
-
-		// TODO - START ;
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double lat = Double.parseDouble(latitudeStr);
+		double lon = Double.parseDouble(longitudeStr);
+		double elev = Double.parseDouble(elevationStr);
+		
+		gpspoint = new GPSPoint(toSeconds(timeStr), lat, lon, elev);
+		
+		return gpspoint;
 
-		// OPPGAVE - SLUTT ;
+		 
 	    
 	}
 	
